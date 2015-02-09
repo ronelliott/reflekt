1b900fd1c5c15cd4ed0f207ce935a5bd12d8ea77
Ron Elliott <ron@elliott.im>
2015-02-08 16:37:20 -0800

    Cleaned up codebase.

--- index.js
+++ index.js
@@ -1,3 +1,73 @@
 'use strict';
 
-module.exports = require('./src');
+var isArray = require('util').isArray;
+
+var FN_ARGS        = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
+    FN_ARG_SPLIT   = /,/,
+    FN_ARG         = /^\s*(_?)(\S+?)\1\s*$/,
+    STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+function ObjectResolver(items) {
+    return function resolve(name) {
+        return items[name];
+    };
+}
+
+function args(fn) {
+    var matches = fn
+        .toString()
+        .replace(STRIP_COMMENTS, '')
+        .match(FN_ARGS);
+
+    if (matches) {
+        return matches[1]
+            .split(FN_ARG_SPLIT)
+            .map(function(arg) {
+                return arg.replace(FN_ARG, function(all, underscore, name) {
+                    return name;
+                });
+            });
+    }
+
+    return [];
+}
+
+function injections(fn, resolver) {
+    var params = isArray(fn) ? fn : args(fn);
+    return params.map(resolver.bind(resolver));
+}
+
+function decorate(fn, resolver, context) {
+    var params = [];
+
+    if (isArray(fn)) {
+        params = injections(fn.slice(0, fn.length - 1), resolver);
+        fn = fn.slice(fn.length - 1)[0];
+    } else {
+        params = injections(fn, resolver);
+    }
+
+    return function() {
+        fn.apply(context || fn, params);
+    };
+}
+
+function call(fn, resolver, context) {
+    var func = decorate(fn, resolver, context);
+    func();
+    return func;
+}
+
+function caller(resolver) {
+    return function caller(fn, context) {
+        call(fn, resolver, context);
+    };
+}
+
+module.exports = {
+    ObjectResolver: ObjectResolver,
+    args:           args,
+    decorate:       decorate,
+    call:           call,
+    caller:         caller,
+};
--- src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var extend = require('extend'),
-    api = require('./api'),
-    resolvers = require('./resolvers');
-
-extend(module.exports, api, resolvers);
