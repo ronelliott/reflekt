6b46fa2b10bb4595821605c1c59760809efe36ab
Ron Elliott <ron@elliott.im>
2015-03-09 11:50:06 -0700

    Added bind polyfill to fix issue with PhantomJS.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict';
 
+// some older JS engines (like PhantomJS 1.9.X and below) do not include the `bind` Function method
+// this polyfill was taken from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Polyfill
+if (!Function.prototype.bind) {
+    Function.prototype.bind = function(oThis) {
+        if (typeof this !== 'function') {
+            // closest thing possible to the ECMAScript 5
+            // internal IsCallable function
+            throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+        }
+
+        var aArgs   = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP    = function() {},
+                fBound  = function() {
+                    return fToBind.apply(this instanceof fNOP
+                                 ? this
+                                 : oThis,
+                                 aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+        fNOP.prototype = this.prototype;
+        fBound.prototype = new fNOP();
+
+        return fBound;
+    };
+}
+
 function isArray(item) {
     return Array.isArray(item) || (typeof item === 'object' && Object.prototype.toString.call(item) === '[object Array]');
 }
